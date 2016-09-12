3c037a3c2d02fef37b2e8ea668441c7575256e24
AlexW2013 <wang4112@umn.edu>
2016-09-11 23:04:05 -0500

    Adding item 19

--- src/chapter4_classes_and_interfaces/Item19UserInterfacesToDefineTypes.java
+++ src/chapter4_classes_and_interfaces/Item19UserInterfacesToDefineTypes.java
@@ -1,7 +1,24 @@
 package chapter4_classes_and_interfaces;
 
-/**
- * Created by Alex on 9/11/2016.
+/*
+    Never use a constant interface; an interface where no methods are defined, only constants.
+    Instead, use utility classes that contain these constants.
+
+    An interface should only be used to show what instances of a class can do.
  */
 public class Item19UserInterfacesToDefineTypes {
+
+    //Dont do this!!
+    interface exampleConstantInterface {
+        //Reminder: all variables are implicitly static final for interfaces
+        static final int secondsPerHour = 3600;
+
+        int minutesPerHour = 60;
+    }
+
+    //Instead, use a utility class as a component
+    class exampleUtilityClass {
+        static final int secondsPerHour = 3600;
+        static final int minutesPerHour = 60;
+    }
 }
