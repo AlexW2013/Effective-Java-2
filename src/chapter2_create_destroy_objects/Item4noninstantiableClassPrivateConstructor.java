c350e5892a59858ea657ebcba0222a6a674fa58b
AlexW2013 <wang4112@umn.edu>
2016-09-11 23:11:22 -0500

    Updating gitignore and a class file

--- src/chapter2_create_destroy_objects/Item4noninstantiableClassPrivateConstructor.java
+++ src/chapter2_create_destroy_objects/Item4noninstantiableClassPrivateConstructor.java
@@ -5,10 +5,10 @@ package chapter2_create_destroy_objects;
    else outside of the class itself can call its constructor due to the private tag
  */
 
-public class Item4NonInstantiableClassPrivateConstructor {
+public class Item4noninstantiableClassPrivateConstructor {
 
     //Nobody else can call this constructor other than this class, so the class can never be instantiated
-    private Item4NonInstantiableClassPrivateConstructor(){
+    private Item4noninstantiableClassPrivateConstructor(){
 
     }
     //A side effect of this is that this class can never be subclassed, as all child constructors
