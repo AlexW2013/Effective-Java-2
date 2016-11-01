46b060a4b8caba984e091c54e26f63888d253585
AlexW2013 <wang4112@umn.edu>
2016-10-31 23:14:25 -0500

    Adding item 47

---
+++ src/chapter8_general_programming/Item47KnowAndUseTheLibraries.java
@@ -0,0 +1,15 @@
+package chapter8_general_programming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    You should never reinvent the wheel; the common libraries were created by experts, and it is
+    wise to take advantage of their experience whenever you need to do something.
+
+    Make sure to review any library additions with a new release.
+ */
+public class Item47KnowAndUseTheLibraries {
+
+
+}
